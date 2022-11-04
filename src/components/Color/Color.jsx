f90fdc53c9ee642649fe2439f655fca21ce4f01b
Murad Ibragimov <texet0598@gmail.com>
2022-11-04 22:07:29 +0300

    added color-card

--- src/components/Color/Color.jsx
+++ src/components/Color/Color.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Color.module.scss';
+import { getRandomInt } from '../Colors/Colors';
 
-function Color() {
+function Color({ myColor }) {
+  function getRGB(c) {
+    return parseInt(c, 16) || c
+  }
+
+  function getsRGB(c) {
+    return getRGB(c) / 255 <= 0.03928
+      ? getRGB(c) / 255 / 12.92
+      : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4)
+  }
+
+  function getLuminance(hexColor) {
+    return (
+      0.2126 * getsRGB(hexColor.substr(1, 2)) +
+      0.7152 * getsRGB(hexColor.substr(3, 2)) +
+      0.0722 * getsRGB(hexColor.substr(-2))
+    )
+  }
+
+  function getContrast(f, b) {
+    const L1 = getLuminance(f)
+    const L2 = getLuminance(b)
+    return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05)
+  }
+
+  function getTextColor(bgColor) {
+    const whiteContrast = getContrast(bgColor, '#ffffff')
+    const blackContrast = getContrast(bgColor, '#000000')
+
+    return whiteContrast > blackContrast ? '#ffffff' : '#000000'
+  }
+  const [thisColor, setThisColor] = useState(myColor)
+  const thisIndex = getRandomInt(0, thisColor.length);
   return (
-    <div className={styles.color}>
-      
+    <div
+      className={styles.color}
+      style={{
+        'backgroundColor': `${thisColor ? thisColor[thisIndex].rgb : null}`,
+        'color': `${getTextColor(thisColor[thisIndex].hex)}`
+      }}
+    >
+      <h2 className={styles['this-color']}>{thisColor[thisIndex].hex}</h2>
+      <h3 className={styles['color-name']}>{thisColor[thisIndex].name}</h3>
     </div>
   );
 }
--- src/components/Colors/Colors.jsx
+++ src/components/Colors/Colors.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Colors.module.scss';
-import {allColors} from '../../colorDataBase';
+import { allColors } from '../../colorDataBase';
 import Color from '../Color/Color';
 
+export function getRandomInt(min = 0, max = 10) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+const generateArray = (length, max) => {
+  const rands = [];
+  while (rands.length < length) {
+    const r = Math.floor(Math.random() * max);
+    if (rands.indexOf(r) === -1) { rands.push(r) }
+  }
+  return rands;
+};
+
+
+
 function Colors() {
-  console.log(allColors[0])
+
+  const [colorIndex1, setColorsIndex1] = useState(getRandomInt());
+  const [colorIndex2, setColorsIndex2] = useState(getRandomInt());
+  const [colorIndex3, setColorsIndex3] = useState(getRandomInt());
+
+  const [colorsArray, setColorsArray] = useState(generateArray(3, allColors[colorIndex3].length - 1));
+  const spaceBarHandler = (e) => {
+    if (e.code === 'Space') {
+      setColorsIndex1(getRandomInt());
+      setColorsIndex2(getRandomInt());
+      setColorsIndex3(getRandomInt());
+      setColorsArray(generateArray(3, allColors.length - 1));
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('keypress', spaceBarHandler);
+    return () => {
+      document.removeEventListener('keypress', spaceBarHandler);
+    }
+  }, [])
+
+
   return (
     <div className={styles.container}>
-      <Color/>
-      <Color/>
-      <Color/>
-      <Color/>
-      <Color/>
+      <Color
+        myColor={allColors[colorIndex3]}
+      />
+      <Color
+        myColor={allColors[colorIndex3]}
+      />
+      <Color
+        myColor={allColors[colorIndex3]}
+      />
+      <Color
+        myColor={allColors[colorIndex1]}
+      />
+      <Color
+        myColor={allColors[colorIndex2]}
+      />
     </div>
   );
 }
